04344d00c5d6e7e624eb9dc48953cde042485ab9
Dyka21 <andikakhaerulana21@gmail.com>
2025-05-10 17:12:16 +0700

    DIKIT

--- pertemuan8/Matakuliah.java
+++ pertemuan8/Matakuliah.java
@@ -1,31 +1,38 @@
 package pertemuan8;
+
 public class Matakuliah {
     private String kode;
     private String nama;
     private int sks;
 
+    // Constructor
     public Matakuliah(String kode, String nama, int sks) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
     }
-
-    // Getter (opsional, kalau mau akses per atribut)
+    // ===== Getter =====
     public String getKode() {
         return kode;
     }
-
     public String getNama() {
         return nama;
     }
-
     public int getSks() {
         return sks;
     }
-
-    // Method untuk menampilkan data (dipakai di list)
-    public void tampil() {
-        System.out.println("Kode: " + kode + ", Nama: " + nama + ", SKS: " + sks);
+    // ===== Setter =====
+    public void setKode(String kode) {
+        this.kode = kode;
     }
-    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+    // ===== tampil() =====
+    public void tampil() {
+        System.out.println("Matakuliah :" + kode + " " + nama + "," + sks);
+    }
 }
